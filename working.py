55fa242de0592f46dd97017ca321c3bebb0dc307
jackg9 <jackgordon9@verizon.net>
2014-12-11 21:09:24 -0500

    Update working.py

--- working.py
+++ working.py
@@ -1,3 +1,9 @@
+import sys
+
+def main(argv)
+
+#TODO: Check command line arguments, if 1 and 2 are the same, it is the last file so print code for first language.
+
 print "Hello, world!"
 
 c = "%CCODE%";
@@ -5,5 +11,17 @@ cpp = "%CPPCODE%";
 java = "%JAVACODE%";
 python = "%PYTHONCODE%";
 
+# Writing CPP code to file.
+file = open('newfile.txt', 'w')
+file.write(cpp)
+
+# TODO: Compile and run CPP code with sys command.
+
+
 # Source: http://cs.lmu.edu/~ray/notes/quineprograms/
 s='s=%r;print s%%s,';print s%s,
+
+file.close()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
